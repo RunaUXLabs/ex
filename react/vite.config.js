296ad95abdc060df88c06463bf4611c714280cef
Runa <runa.uxlabs@gmail.com>
2024-12-05 19:33:15 +0900

    수동 업데이트 환경 설정 성공

--- react/vite.config.js
+++ react/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react/', // GitHub Pages에서 react 프로젝트의 경로
+  base: '/ex/react/', // GitHub Pages에서 react 프로젝트의 경로
   build: {
     outDir: '../dist/react', // 상위 디렉토리의 dist/react에 빌드
     emptyOutDir: false,
--- react2/vite.config.js
+++ react2/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react2/', // GitHub Pages에서 react2 프로젝트의 경로
+  base: '/ex/react2/', // GitHub Pages에서 react2 프로젝트의 경로
   build: {
     outDir: '../dist/react2', // 상위 디렉토리의 dist/react2에 빌드
     emptyOutDir: false,
