1a972f8743ec1537b4467069e8f6676575e2ab77
Runa <runa.uxlabs@gmail.com>
2024-12-05 14:47:46 +0900

    수동방식으로 설정 변경

--- react/vite.config.js
+++ react/vite.config.js
@@ -5,10 +5,4 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   base: '/ex/react/',
-  build: {
-    outDir: 'dist', // 빌드 디렉토리
-    rollupOptions: {
-      input: '/index.html', // 진입 파일
-    },
-  },
 });
