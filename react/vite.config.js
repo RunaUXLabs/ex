aa5c63c5405c0b5c2dd812f55110d6749e93771d
Runa <runa.uxlabs@gmail.com>
2024-12-05 15:34:37 +0900

    초기화

--- react/vite.config.js
+++ react/vite.config.js
@@ -5,11 +5,4 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   base: '/ex/react/',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
 });
