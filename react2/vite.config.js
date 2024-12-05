77f0ed67a161e62432e6ee9521fdc8b444d6a860
Runa <runa.uxlabs@gmail.com>
2024-12-05 18:05:11 +0900

    루트에 하위 디렉토리 빌드가 디플로이되도록 환경구성

--- react2/vite.config.js
+++ react2/vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ex/react2/',
   build: {
-    outDir: "dist",
-    assetsDir: "assets",
+    outDir: '../../dist/react2', // 빌드 결과를 루트의 dist/react2로 설정
+    emptyOutDir: true, // 기존 파일 삭제
   },
 });
