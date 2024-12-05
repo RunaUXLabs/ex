f07e3031e4aaafd60f6ab5526f07ec5d03f18064
Runa <runa.uxlabs@gmail.com>
2024-12-05 17:29:43 +0900

    리액트 프로젝트 2개 추가하여 환경설정, 각 프로젝트에서 빌드한 파일을 루트로 복제하는 구조 구성

--- react2/vite.config.js
+++ react2/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ex/react/',
+  base: '/ex/react2/',
   build: {
     outDir: "dist",
     assetsDir: "assets",
