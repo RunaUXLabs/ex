56dba5f631e01ef49de1b438e31f985a3c323052
Runa <runa.uxlabs@gmail.com>
2024-12-05 19:06:55 +0900

    각프로젝트에서 빌드와 디플로이를 하면 루트에 dist가 생겨 배포되는 환경

--- react/vite.config.js
+++ react/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ex/react/', // GitHub Pages에서 react 프로젝트의 경로
+  base: '/react/', // GitHub Pages에서 react 프로젝트의 경로
   build: {
-    outDir: "dist",
-    assetsDir: "assets",
+    outDir: '../dist/react', // 상위 디렉토리의 dist/react에 빌드
+    emptyOutDir: false,
   },
 });
