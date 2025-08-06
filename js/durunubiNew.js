326e3e9b2484e9c73fd3b7d47530268cd784373e
Runa <runa.uxlabs@gmail.com>
2025-08-06 23:47:26 +0900

    함수 최적화 및 리팩토링

--- js/durunubiNew.js
+++ js/durunubiNew.js
@@ -1,5 +1,6 @@
 // durunubiNew
 
+// Swiper 객체 초기화
 var banner = new Swiper("#banner", {
 	slidesPerView: 1,
 	pagination: {
@@ -18,60 +19,82 @@ var banner = new Swiper("#banner", {
 });
 
 
-// 길종류검색 탭구조
-let roadSearchButtons = document.querySelectorAll('#roadSearch > button');
-Array.from(roadSearchButtons).forEach((eachButton, index) => {
-	eachButton.addEventListener('click', function () {
-		let tabBoxs = document.querySelectorAll('#roadResult > div');
-		for (var i = 0; i < tabBoxs.length; i++) {
-			tabBoxs[i].classList.remove("on");
-			roadSearchButtons[i].classList.remove("on");
-		}
-		tabBoxs[index].classList.add("on");
-		this.classList.add("on");
-	});
-});
-roadSearchButtons[0].click();
+// 탭설정 함수
+const setupTabs = (buttonSelector, contentSelector, activeClass = "on") => {
+	const buttons = document.querySelectorAll(buttonSelector);
+	const contents = document.querySelectorAll(contentSelector);
 
-// 해파랑/남파랑/서해랑길 탭구조
-let roadButtons = document.querySelectorAll('#roadButtons > h2');
-Array.from(roadButtons).forEach((eachButton, index) => {
-	eachButton.addEventListener('click', function () {
-		let roadLists = document.querySelectorAll('#roadList > div');
-		let tabLinks = document.querySelectorAll('#roadButtons > a');
-		for (var i = 0; i < roadLists.length; i++) {
-			roadLists[i].classList.remove("on");
-			roadButtons[i].classList.remove("on");
-			tabLinks[i].classList.remove("on");
-		}
-		roadLists[index].classList.add("on");
-		tabLinks[index].classList.add("on");
-		this.classList.add("on");
+	buttons.forEach((button, index) => {
+		button.addEventListener("click", () => {
+			buttons.forEach((btn, i) => {
+				btn.classList.remove(activeClass);
+				contents[i].classList.remove(activeClass);
+			});
+			button.classList.add(activeClass);
+			contents[index].classList.add(activeClass);
+		});
 	});
-});
-roadButtons[0].click();
+
+	buttons[0].click(); // 초기화
+};
+// 길종류검색 탭구조
+setupTabs("#roadSearch > button", "#roadResult > div");
+// 해파랑/남파랑/서해랑길 탭구조
+setupTabs("#roadButtons > h2", "#roadList > div");
+
 
 // 한국관광공사 두루누비 API
-const serviceKey = ''; // API인증키
-const haeRoad = encodeURIComponent(`해파랑길`); //%ED%95%B4%ED%8C%8C%EB%9E%91%EA%B8%B8
-const namRoad = encodeURIComponent(`남파랑길`); //%EB%82%A8%ED%8C%8C%EB%9E%91%EA%B8%B8
-const seoRoad = encodeURIComponent(`서해랑길`); //%EC%84%9C%ED%95%B4%EB%9E%91%EA%B8%B8
+const serviceKey = 'Iv76qim5Ny6HUJz794fWsPnAJ%2BwHb%2BsdheosUWgR8rbILCn7M9L6zNFDr%2FHWJtuGhpLXdiP%2FfvROWpDUW7Zupg%3D%3D'; // API인증키
+const createAPIUrl = (roadName, serviceKey) => {
+	return `https://apis.data.go.kr/B551011/Durunubi/courseList?serviceKey=${serviceKey}&_type=json&numOfRows=100&pageNo=1&MobileOS=ETC&MobileApp=AppTest&crsKorNm=${encodeURIComponent(roadName)}&routeIdx=%20&brdDiv=DNWW`;
+};
 
-// 개인계정의 인증키로 활성화된 API입력
-const publicAPIurl1 = `https://apis.data.go.kr/B551011/Durunubi/courseList?numOfRows=11&pageNo=1&MobileOS=ETC&MobileApp=Test&serviceKey=${serviceKey}&_type=json&crsKorNm=${haeRoad}&brdDiv=DNWW`;
-const publicAPIurl2 = `https://apis.data.go.kr/B551011/Durunubi/courseList?numOfRows=11&pageNo=1&MobileOS=ETC&MobileApp=Test&serviceKey=${serviceKey}&_type=json&crsKorNm=${namRoad}&brdDiv=DNWW`;
-const publicAPIurl3 = `https://apis.data.go.kr/B551011/Durunubi/courseList?numOfRows=11&pageNo=1&MobileOS=ETC&MobileApp=Test&serviceKey=gptlfeF0tckKWZugnRmyvS4wuIp7K2dDyLC9SjDzp%2BIk8HEEiN8OH9m9Xm1%2BH%2FIvBWPxcRBQ8mhpUGvA9n9yVA%3D%3D&_type=json&crsKorNm=%EC%84%9C%ED%95%B4%EB%9E%91%EA%B8%B8&brdDiv=DNWW`;
-
-const publicAPIurl = [publicAPIurl1, publicAPIurl2, publicAPIurl3];
-
-// json
-// https://apis.data.go.kr/B551011/Durunubi/courseList?numOfRows=11&pageNo=1&MobileOS=ETC&MobileApp=AppTest&serviceKey=서비스키_type=json&crsKorNm=%ED%95%B4%ED%8C%8C%EB%9E%91%EA%B8%B8&brdDiv=DNWW
+const publicAPIurl = [
+	createAPIUrl("해파랑길", serviceKey),
+	createAPIUrl("남파랑길", serviceKey),
+	createAPIUrl("서해랑길", serviceKey),
+];
 
 
-// 3개 길 코스정보 추출 추천걷기길 카드 만들기
+// 랜덤 숫자 생성함수(뽑을 갯수, 최대값(배열길이), 중복여부)
+const generateRandomNumbers = (n, max, unique = false) => {
+	const numList = unique ? new Set() : [];
+	while (unique ? numList.size < n : numList.length < n) {
+		const randomNum = Math.floor(Math.random() * max);
+		unique ? numList.add(randomNum) : numList.push(randomNum);
+	}
+	return [...numList];
+};
+// 간단한 클래스 토글 함수
+const toggleClass = (selector, className, add = true) => {
+	const elements = document.querySelectorAll(selector);
+	elements.forEach(element => {
+		add ? element.classList.add(className) : element.classList.remove(className);
+	});
+};
+// 요소생성하여 내용과 클래스명 넣는 함수
+function createElementWithText(tag, text, className) {
+	const element = document.createElement(tag);
+	if (className) element.classList.add(className);
+	if (text) {
+		const textNode = document.createTextNode(text);
+		element.appendChild(textNode);
+	}
+	return element;
+}
+
+
+// 3개 길 코스정보 추출 추천걷기길 카드정 만들기(num = 0:해파랑, 1:남파랑, 2:서해랑)
 const getInfo = async (num) => {
-	await fetch(`${publicAPIurl[num]}`).then((response) => response.json()).then((json) => {
+	try {
+		const response = await fetch(publicAPIurl[num]);
+		if (!response.ok) {
+			console.error(`Error: ${response.status} - ${response.statusText}`);
+			return;
+		}
+		const json = await response.json();
 		let data = json.response.body.items.item;
+		// console.log(data);
 
 		// 해당길 인덱스번호 입력하여 길 추출하여 정보구분, 예) 해파랑길 1코스 => index = 1
 		var crs = (index) => data[index]; // 해당길의 인덱스별 길정보 반환하여 특정
@@ -79,23 +102,16 @@ const getInfo = async (num) => {
 		let crsDistance = (index) => `${crs(index)['crsDstnc']} km`;  // 특정길 거리
 		// 특정길 소요시간 mmmm분 => h시간 mm분으로 가공
 		let crsHourWithMin = (index) => {
-			let crsTimeMinute = (index) => Number(crs(index)['crsTotlRqrmHour']); // 소요시간(분)
-			// console.log(crsTimeMinute(index));
-			let crsTimeHour = (crsTimeMinute(index) / 60).toFixed(2); // 소요시간(h.mm)
-			let result = '';
-			for (i = 0; i < crsTimeHour.length; i++) {
-				arr = [crsTimeHour.charAt(crsTimeHour.length - (i + 1))]; // for구문을 통해 반복하여 역순으로 배열채우기
-				arr = arr.filter((element) => element !== '.'); // 소수점 삭제
-				str = '';
-				if (arr != '') str += arr;
-				if (i == 3) str += '시간 ';
-				if (i == 0) str += '분';
-				result = str + result;
-			}
-			// 00분 일 경우 시간만 나올수 있도록 제거
-			let zeroComma = result.search(/00/gi); // 천억단위에서 0값 및 콤마찾기, 있으면 index 반환
-			if (zeroComma > 0) result = result.replace('00분', '');
-			return result;
+			const totalMinutes = Number(crs(index)['crsTotlRqrmHour']); // 소요시간(분)
+			const hours = Math.floor(totalMinutes / 60); // 시간 계산
+			const minutes = totalMinutes % 60; // 분 계산
+
+			// 시간과 분을 조합하여 반환
+			return hours > 0
+				? minutes > 0
+					? `${hours}시간 ${minutes}분`
+					: `${hours}시간`
+				: `${minutes}분`;
 		};
 		// crsLevel 코스레벨
 		let crsLevel = (index) => {
@@ -106,96 +122,60 @@ const getInfo = async (num) => {
 			if (level == 3) result = '어려움';
 			return result;
 		};
-		let crsArea = (index) => crs(index)['sigun'];  // 특정길 코스위치
-		let crsSummary = (index) => crs(index)['crsSummary'];  // 특정길 코스정보 요약
-
-		// 각 길정보 데이터 디스트럭처링
-		function createRecomandRoad(index) {
-			var a = document.createElement('a');
-			// 상단의 num 번호에 따라 걷기길 분류되니, 그에 따라 클래스 이름변경
-			if (num == 0) a.classList.add(`hae`, `hae${index}`);
-			else if (num == 1) a.classList.add(`nam`, `nam${index}`);
-			else if (num == 2) a.classList.add(`seo`, `seo${index}`);
-
-			// 거리/시간/난이도
-			function makeRoadInfo(index) {
-				var road = document.createElement('div');
-				let spanDistance = document.createElement('span');
-				let spanTime = document.createElement('span');
-				let spanLevel = document.createElement('span');
-				let spanDistanceTxt = document.createTextNode(crsDistance(index));
-				let spanTimeTxt = document.createTextNode(crsHourWithMin(index));
-				let spanLevelTxt = document.createTextNode(crsLevel(index));
-
-				spanDistance.appendChild(spanDistanceTxt);
-				spanTime.appendChild(spanTimeTxt);
-				spanLevel.appendChild(spanLevelTxt);
-
-				road.appendChild(spanDistance);
-				road.appendChild(spanTime);
-				road.appendChild(spanLevel);
-				road.classList.add('road');
-
-				a.appendChild(road);
-			}
-			// 나머지
-			function makeBottomInfo(index) {
-				let area = document.createElement('span');
-				let h4 = document.createElement('h4');
-				let summary = document.createElement('span');
-				area.classList.add('area');
-				summary.classList.add('summary');
-
-				let areaTxt = document.createTextNode(crsArea(index));
-				let h4Txt = document.createTextNode(crsName(index));
-				let summaryTxt = document.createTextNode(crsSummary(index));
-
-				area.appendChild(areaTxt);
-				h4.appendChild(h4Txt);
-				summary.appendChild(summaryTxt);
-
-
-				a.appendChild(area);
-				a.appendChild(h4);
-				a.appendChild(summary);
-			}
-			makeRoadInfo(index);
-			makeBottomInfo(index);
-
-			// 추천길을 a에 집어넣기
-			let block = document.querySelector(".recomandRoad");
-			block.appendChild(a);
+		let crsArea = (index) => crs(index)['sigun']; // 특정길 코스위치
+		// 특정길 코스정보 요약(데이터에 있는 <br> 태그와 하이픈(-) 제거)
+		let crsSummary = (index) => {
+			let summary = crs(index)['crsSummary'];
+			return summary.replace(/<br\s*\/?>(\s*-\s*)?|-/gi, '');
 		};
-		// 각 길 랜덤추첨
-		let random = [...randomNum(5)];
-		console.log(random);
-		for (const index of random) createRecomandRoad(index);
-	});
+
+		// 각 길정보 데이터 디스턱쳐링
+		function createRecomandRoad(index) {
+			const a = document.createElement('a');
+
+			// 상단의 num 번호에 따라 걷기길 분류되니, 그에 따라 클래스 이름변경
+			if (num === 0) a.classList.add(`hae`, `hae${index}`);
+			else if (num === 1) a.classList.add(`nam`, `nam${index}`);
+			else if (num === 2) a.classList.add(`seo`, `seo${index}`);
+
+			// 길상세 정보 뱃지
+			const road = document.createElement('div');
+			road.classList.add('road');
+			road.append(
+				createElementWithText('span', crsDistance(index)),
+				createElementWithText('span', crsHourWithMin(index)),
+				createElementWithText('span', crsLevel(index))
+			);
+			a.appendChild(road);
+
+			// 길 전체 정보
+			a.append(
+				createElementWithText('span', crsArea(index), 'area'),
+				createElementWithText('h4', crsName(index)),
+				createElementWithText('span', crsSummary(index), 'summary')
+			);
+
+			// 만들어진 카드 html에 추가
+			const block = document.querySelector('.recomandRoad');
+			block.appendChild(a);
+		}
+
+		// 각 길마다 5개씩 랜덤추첨(중복허용)
+		const uniqueRandom = generateRandomNumbers(5, 10, true);
+		for (const index of uniqueRandom) createRecomandRoad(index);
+
+	} catch (error) {
+		console.error("data fetch 실패:", error);
+	}
 };
 
-// 0:해파랑, 1:남파랑, 2:서해랑 중 10번 숫자뽑기
-const randomNum123 = (n) => {
-	let numList = [];
-	while (numList.length < n) numList.push(Math.floor(Math.random() * 3)); // +1 안함
-	return numList;
-};
-
-// 중복없는랜덤숫자뽑기
-const randomNum = (n) => {
-	let numList = new Set();
-	while (numList.size < n) numList.add(Math.floor(Math.random() * 10) + 1);
-	return numList;
-};
-
-// fetch API, async/await 여러개 연결하기
+// 각 길정보 추출하여 카드 만들기
 const getRecomandRoad = async () => {
-	// 랜덤카드 15장뽑기
-	let randomCard = randomNum123(3);
-	console.log(randomCard);
-	for (const num of randomCard) await getInfo(num);
+	// 0:해파랑, 1:남파랑, 2:서해랑 중 세번뽑기 (중복허용)
+	const randomCard = generateRandomNumbers(3, 3);
+	await Promise.all(randomCard.map(num => getInfo(num)));
 
 	// 카드 다 출력되면 css로 .recomandRoad 안의 로티파일 제거하기
-	const recomandRoad = document.querySelector('.recomandRoad');
-	recomandRoad.classList.add('on');
+	toggleClass('.recomandRoad', 'on');
 };
 getRecomandRoad();
