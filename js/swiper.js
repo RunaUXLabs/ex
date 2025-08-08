fb06826c905f453f210b39590a2e81fd3ec325fc
Runa <runa.uxlabs@gmail.com>
2025-08-08 11:19:37 +0900

    swiper 기본 샘플

---
+++ js/swiper.js
@@ -0,0 +1,13 @@
+
+
+
+var swiper = new Swiper(".my", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+});
